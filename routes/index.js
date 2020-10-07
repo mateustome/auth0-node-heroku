ee53e275b73ab460c52e649e2b6ecd7cf7b92a85
mateustome <63316009+mateustome@users.noreply.github.com>
2020-10-07 14:03:06 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const pg = require("pg");
+pg.defaults.ssl = true;
 
+var dbString = process.env.DATABASE_URL;
+
+var sharedPgClient;
+
+pg.connect(dbString, function(err,client){
+  if(err){
+      console.error("PG Connection Error")
+  }
+  console.log("Connected to Postgres");
+  sharedPgClient = client;
+});
 /* GET home page. */
 router.get('/', function (req, res, next) {
   var query = "SELECT * FROM salesforce.contacts";
